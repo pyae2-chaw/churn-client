122abfc65cd90d1cc0495421b0f9e5f448d3953e
Pyae Kyi Thar Chaw <pyae2.chaw@live.uwe.ac.uk>
2025-08-13 12:51:39 +0100

    dotenv fix

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -19,7 +19,7 @@ import {
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Navbar from "../components/Navbar.jsx";
-import mlApi from "./lib/mlApi.js";
+import mlApi from ".mlApi.js";
 
 const checklistItems = [
   "File must be in CSV format (.csv)",
